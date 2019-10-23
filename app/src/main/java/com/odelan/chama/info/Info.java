0275cb14b329c130808bbf387c1392629ec65c28
jwrunner3224 <joybrother2019@gmail.com>
2019-10-22 20:03:27 -0800

    version 1.0 complete.

---
+++ app/src/main/java/com/odelan/chama/info/Info.java
@@ -0,0 +1,25 @@
+package com.odelan.chama.info;
+
+public class Info {
+
+//    public static String SUPPORT_PHONE = "254720515026";
+    public static String SUPPORT_PHONE = "+254799171355";
+    public static String SUPPORT_EMAIL = "support@chamafinance.com";
+
+    // Mode
+    public static final String MODE_TEST = "test_mode";
+    public static final String MODE_LIVE = "live_mode";
+    public static String MODE = MODE_LIVE;
+
+    // URL
+    public static String BASE_API_URL = "https://api.chamaplus.com/";
+    public static String BASE_API_URL_B2C = "https://api.chamaplus.com:8886/";
+    public static String BASE_SOCKET_URL = "https://api.chamaplus.com:8889/";
+
+//    public static String BASE_API_URL = "https://localhost:8888/";
+//    public static String BASE_API_URL = "http://192.168.110.62:8888/";
+//    public static String BASE_API_URL_B2C = "http://192.168.110.62:8886/";
+//    public static String BASE_SOCKET_URL = "http://192.168.110.62:8889/";
+
+    // constant
+}
